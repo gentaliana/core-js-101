efdd0fbcc827d8a416f329ebca029fd4ad3c2282
gentaliana <gentaliana@gmail.com>
2020-12-12 23:16:29 +0400

    feat: resolve task

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -38,8 +38,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -53,8 +53,13 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = Object.create(proto);
+  const result = JSON.parse(json);
+  Object.keys(result).forEach((i) => {
+    obj[i] = result[i];
+  });
+  return obj;
 }
 
 /**
